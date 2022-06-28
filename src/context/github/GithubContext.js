17e513e62e9bb51a7786672bf2d070e20281d6a5
Diyor End <diyorend@gmail.com>
2022-06-28 08:54:15 +0500

    from search to clear | awesome

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,33 +8,48 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   };
 
   //reducer
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  //fetch users
-  const fetchUsers = async () => {
-    const response = await fetch(`${GITHUB_URL}/users`, {
+  //Get search results
+  const searchUsers = async (text) => {
+    setLoading();
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
 
-    const data = await response.json();
+    const { items } = await response.json();
     dispatch({
       type: "GET_USERS",
-      payload: data,
+      payload: items,
     });
   };
+
+  //clear users from state
+  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
+
+  //Set loading
+  const setLoading = () =>
+    dispatch({
+      type: "SET_LOADING",
+    });
 
   return (
     <GithubContext.Provider
       value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
+        clearUsers,
       }}
     >
       {children}
